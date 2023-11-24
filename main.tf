f92e1e54c205155c6301236ea636f0f9e13cb45a
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-24 20:03:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 resource "azurerm_resource_group" "example02" {
  name     = "example"
 location = "West Europe"
